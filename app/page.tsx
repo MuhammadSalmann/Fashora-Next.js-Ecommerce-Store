df324d602610b31ac19d76aaeccc28a4fb927c4b
muhammadsalmann <salmansidd372@gmail.com>
2025-06-08 02:15:54 +0500

    Fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,14 @@
+import { Button } from "@/components/ui/button";
+
 const Homepage = () => {
-  return ( <>Fashora</> );
+  return ( <>
+  <div className="wrapper">
+
+  <Button className="cursor-pointer">Click em</Button>
+
+  </div>
+  
+  </> );
 }
  
 export default Homepage;
