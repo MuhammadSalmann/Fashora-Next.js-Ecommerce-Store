1712b463fca5c6cfd2c91a3bcca761ab33115868
muhammadsalmann <salmansidd372@gmail.com>
2025-09-26 00:08:41 +0500

    setup prisma and fetched products from prisma serverless actions

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,25 +1,23 @@
 import ProductList from '@/components/shared/product/product-list';
-import sampleData from '@/db/sample-data';
-// import {
-//   getLatestProducts,
-//   getFeaturedProducts,
-// } from '@/lib/actions/product.actions';
+import {
+  getLatestProducts,
+  // getFeaturedProducts,
+} from '@/lib/actions/product.actions';
 // import ProductCarousel from '@/components/shared/product/product-carousel';
 // import ViewAllProductsButton from '@/components/view-all-products-button';
 // import IconBoxes from '@/components/icon-boxes';
 // import DealCountdown from '@/components/deal-countdown';
 
 const Homepage = async () => {
-  // const latestProducts = await getLatestProducts();
+  const latestProducts = await getLatestProducts();
   // const featuredProducts = await getFeaturedProducts();
-  const sampleProducts = sampleData.products;
 
   return (
     <>
       {/* {featuredProducts.length > 0 && (
         <ProductCarousel data={featuredProducts} />
       )} */}
-      <ProductList data={sampleProducts} title='Newest Arrivals' limit={4} />
+      <ProductList data={latestProducts} title='Newest Arrivals' limit={4} />
       {/* <ViewAllProductsButton />
       <DealCountdown />
       <IconBoxes /> */}
